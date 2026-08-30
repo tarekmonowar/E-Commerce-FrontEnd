128713c95e7ffb57a7143def174690e8cebee9cc
Tarek Monowar <tarekmonowar353@gmail.com>
2026-08-30 12:33:19 +0600

    style: lenis smooth scroll added

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
 import { StrictMode } from "react";
 import { Provider } from "react-redux";
 import { createRoot } from "react-dom/client";
+import { ReactLenis } from "lenis/react";
+import "lenis/dist/lenis.css";
 import "./index.css";
 import App from "./App.tsx";
 import store from "./redux/store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ReactLenis
+      root
+      options={{
+        autoRaf: true,
+        anchors: true,
+        stopInertiaOnNavigate: true,
+      }}
+    >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ReactLenis>
   </StrictMode>,
 );
